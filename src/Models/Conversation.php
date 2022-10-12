22f7e5f87d7e249cece37d21a45dc272910c60ab
Tapiwanashe Mugoniwa <tmugonix@gmail.com>
2022-10-12 06:25:27 +0200

    Include unread count

--- src/Models/Conversation.php
+++ src/Models/Conversation.php
@@ -381,13 +381,13 @@ class Conversation extends BaseModel
                         ->where($this->tablePrefix.'message_notifications.messageable_type', $participant->getMorphClass())
                         ->whereNull($this->tablePrefix.'message_notifications.deleted_at');
                 },
-                'conversation.message_notifications'
-//                => function($query) use ($participant) {
-//                    $query->where('messageable_id', $participant->getKey())
-//                        ->where('messageable_type', $participant->getMorphClass())
-//                        ->where('is_seen', 0);
-//
-//                }
+                'conversation.message_notifications' => function($query) use ($participant) {
+                    $query->where(function ($q) use ($participant) {
+                        $q->where('messageable_id', $participant->getKey())
+                            ->where('messageable_type', $participant->getMorphClass());
+                    })->where('is_seen', false);
+
+                }
             ]);
 
 //        if (isset($options['filters']['include_unread_count']) && $options['filters']['include_unread_count']) {
